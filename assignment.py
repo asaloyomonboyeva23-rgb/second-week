a2575049b90f07fc810eeb4dfbf2423df742eaac
asaloyomonboyeva23-rgb <asaloyomonboyeva23@gmail.com>
2025-10-12 18:17:50 +0500

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,23 @@
+character_name = input("enter the character's name: " )
+character_class = input("enter the character's class: ")
+home_realm = input("enter the character's home: ")
+special_ability = input("enter the character's ability: ")
+choosen_weapon = input("enter the character's weapon : ")
+
+
+print(
+"  ========================================\n"
+"-- CHARACTER CREATION COMPLETE --\n"
+"==========================================\n"
+
+"Name:     " + character_name + "\n"
+"Class:    " + character_class + "\n"
+"home_Realm:    " + home_realm + "\n"
+
+"Title:  " + character_name + " the " + character_class + " of " + home_realm + "\n"
+
+"-------------------------------------------\n"
+
+"Weapon of Choice: " + choosen_weapon + "\n"
+"Special Ability: " + special_ability + "\n"
+"========================================")
